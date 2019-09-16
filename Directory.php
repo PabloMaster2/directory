34adecb15f4e792bc7c0bfc24e0c6f38bcf090b6
PabloMaster2 <55392265+PabloMaster2@users.noreply.github.com>
2019-09-16 14:34:49 +0200

    Add files via upload

---
+++ Directory.php
@@ -0,0 +1,24 @@
+<?php
+function download($patch = 'inne')
+{
+    $katalog = @dir($patch) or die ('Can't read selected folder ');
+	while ($plik_kat = $katalog->read())
+	IF(is_file($patch.'/'.$plik_kat))
+		{
+		echo '- <a href="'.$patch.'/'.$plik_kat.'">'.$plik_kat.'</a><br>';
+		}
+	elseIF(is_dir($patch.'/'.$plik_kat))
+		{
+		echo '- <a href="bug.php?patch='.$patch.'/'.$plik_kat.'"><b>'.$plik_kat.'</b></a><br>';
+		}
+	$katalog->close();
+	}
+
+IF(!isset($_GET['patch']))
+	{
+	download();
+	}
+else
+	{
+	download($_GET['patch']);
+	}
